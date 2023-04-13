a170325892b80635f5e40d5dff16b5b2e2094569
vasy <https://github.com/solomon202/NewsApp>
2023-04-13 22:11:09 +0300

    nine

--- src/main/java/game/Mar/Enter/Game.java
+++ src/main/java/game/Mar/Enter/Game.java
@@ -5,7 +5,8 @@ public class Game implements Runnable {
 	private GameWindow gameWindow;
 	private GamePanel gamePanel;
 	private Thread gameThread;
-	private  final int FPS_SET = 120;
+	private final int FPS_SET = 120;
+	private final int USP_SET = 200;
 	
 	public Game() {
 		gamePanel = new GamePanel();
@@ -26,13 +27,32 @@ public class Game implements Runnable {
 	public void run() {
 		//переменная кадров и время и новое время 
 		double timePerFrame = 1000000000.0/FPS_SET;
+		double timePerUpdate =1000000000.0/USP_SET;
 		long lastFrame  = System.nanoTime();
 		long now = System.nanoTime();
 		
+		long previousTime = System.nanoTime();
+		
+		
+		int frames = 0;
+		int updates = 0;
+		long lastCheck = System.currentTimeMillis();
+		
+		
+		double deltaU = 0;
+		
 		while(true) {
 			now = System.nanoTime();
+			long currentTime = System.nanoTime();
+			deltaU += (currentTime - previousTime)/timePerUpdate;
+			previousTime = currentTime;
+			
+			if (deltaU >=1){
+				updates++;
+				deltaU--;
+			}
+			
 			if(now - lastFrame >= timePerFrame) {
-				
 				gamePanel.repaint();
 				lastFrame = now;
 			}
